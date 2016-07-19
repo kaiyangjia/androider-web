9bcafb8a2c630e4e53326d33e72484584217cd7f
kaiyangjia <kaiyangjia@163.com>
2016-07-19 18:33:48 +0800

    modify readme

--- common/src/main/java/com/jiakaiyang/androider/web/common/tools/InstanceUtils.java
+++ common/src/main/java/com/jiakaiyang/androider/web/common/tools/InstanceUtils.java
@@ -1,6 +1,7 @@
 package com.jiakaiyang.androider.web.common.tools;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * 对实例的成员、方法等进行的常用操作。主要采用反射实现
@@ -32,5 +33,25 @@ public class InstanceUtils {
             }
         }
         return result;
+    }
+
+
+    /**
+     * 获取一个对象指定的方法对象
+     * @param instance
+     * @param methodName
+     * @return
+     */
+    public static Method getInstanceMethod(Object instance,String methodName){
+        Method[] methods = instance.getClass().getDeclaredMethods();
+
+        for (Method method : methods){
+            String name = method.getName();
+            if(methodName.endsWith(name)){
+                return method;
+            }
+        }
+
+        return null;
     }
 }
